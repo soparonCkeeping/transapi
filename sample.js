8a3809e2cedba2c3d2d145eb000e0f8052b845bf
tran quang huy <ieltsaddict@gmail.com>
2021-10-30 11:01:15 +0700

    update

--- sample.js
+++ sample.js
@@ -1,7 +1,7 @@
 const axios = require('axios')
 const API_KEY = `AIzaSyDpuhPAXBFKTzK1P7n9vSW87mZtZBSG408`
 
-axios.post('http:8000/cors-everywhere', 
+axios.post('https://tran-huy-transapi.herokuapp.com/cors-everywhere', 
     {
         requestURL: `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Museum%20of%20Contemporary%20Art%20Australia&inputtype=textquery&fields=formatted_address%2Cname%2Crating%2Copening_hours%2Cgeometry&key=${API_KEY}`
     }
